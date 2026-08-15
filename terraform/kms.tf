807a528dcc5801e72f2c1484348b5de03413516b
bagas rinaldi <cycwag3006@gmail.com>
2026-08-15 16:04:54 +0700

    fiks iac code security #2

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -1,6 +1,38 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "eks" {
   description             = "KMS key untuk enkripsi Kubernetes Secret di EKS"
   deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnableRootAccountAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid       = "AllowEKSClusterRole"
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_iam_role.cluster.arn
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:CreateGrant"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 
   tags = {
     Name = "${var.cluster_name}-eks-secrets-key"
